f5a9ed79de8f302ea78a18c300c51e77f867963f
RD <ronak.dumaniya@gmail.com>
2021-09-07 11:08:24 +0530

    env file changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  URL: 'http://localhost:3001/api',
+  URL: 'https://dashboard8852.herokuapp.com/api',
   facebookAppId: '314930319788683',
   production: false
 };
